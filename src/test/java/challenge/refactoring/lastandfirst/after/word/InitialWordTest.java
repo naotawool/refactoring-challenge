fd49d01792f34ba0240e63b5a42bcd5cc86d6ea5
naotake.k <naotake.87@gmail.com>
2013-03-07 23:57:34 +0900

    change Exception test to ExpectException rule.

--- src/test/java/challenge/refactoring/lastandfirst/after/word/InitialWordTest.java
+++ src/test/java/challenge/refactoring/lastandfirst/after/word/InitialWordTest.java
@@ -3,7 +3,9 @@ package challenge.refactoring.lastandfirst.after.word;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * {@link InitialWord}に対するテストクラス。
@@ -14,19 +16,28 @@ public class InitialWordTest {
 
     private InitialWord testee;
 
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
     public void 単語のごりらを指定してインスタンスを生成できること() {
         testee = InitialWord.wordOf("ごりら");
         assertThat(testee.toString(), is("ご"));
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void 単語を指定なかった場合はNullPointerExceptionが送出されること() {
+        exception.expect(NullPointerException.class);
+        exception.expectMessage(is("頭文字は必須です。"));
+
         InitialWord.wordOf(null);
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void 頭文字を指定なかった場合はNullPointerExceptionが送出されること() {
+        exception.expect(NullPointerException.class);
+        exception.expectMessage(is("頭文字は必須です。"));
+
         InitialWord.of("");
     }
 }
